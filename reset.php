3d2d4f13627579a3972fdcebf19469ab415a1e01
kevinsinjy <40633341+kevinsinjy@users.noreply.github.com>
2018-07-30 00:35:35 +0200

    Create reset.php

---
+++ reset.php
@@ -0,0 +1,11 @@
+<?php
+
+//Exo 5
+    echo "<form action='login.php' method='post'>
+    redéfinissez votre : <br>
+    login
+    <input type='text' name='login'>
+    <br>et votre mot de passe : <br> 
+    <input type='password' name = 'password'>
+    <input type='submit' name='valider'>
+    </form>";
